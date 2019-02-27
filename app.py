bd3e53779e121309c8d3baf3bf94c2cedae55678
felipe <felipelm123@gmail.com>
2019-02-27 16:21:41 -0300

    adding check

--- app.py
+++ app.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python
 
-from flask import Flask, Response, render_template, flash, session, request
+from flask import Flask, Response, render_template, flash, session, request, redirect
 import os
 import datetime
 import jwt
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Oi"
-
-@app.route("/login_error")
-def login_error():
-    return Response('Login Required', 401, {})
-
+@app.route("/check")
+def check():
+    try:
+        if session['logged_in']:
+            return Response('Login OK', 200, {})
+        else:
+            return Response('Login Required', 401, {})
+    except:
+        session['logged_in'] = False
+        return Response('Login Required', 401, {})
 
 @app.route("/login")
 def login():
-    return render_template('login.html')
+    rd = request.args.get('rd')
+
+    return render_template('login.html', rd=rd)
 
 
 @app.route('/login', methods=['POST'])
-def do_admin_login():
+def do_login():
     if request.form['password'] == '123' and request.form['username'] == '123':
         session['logged_in'] = True
+        return redirect(request.form['rd'], code=302)
     else:
         flash('wrong password!')
-    return index()
+    return check()
 
 @app.route("/logout")
 def logout():
     session['logged_in'] = False
-    return index()
+    return check()
 
 if __name__ == '__main__':
     app.secret_key = '29cSy004wj2931m'
