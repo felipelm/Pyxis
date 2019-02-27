361973ee8f37adb0890e1abe4d6049b23e4ba08c
felipe <felipelm123@gmail.com>
2019-02-27 17:11:05 -0300

    adding check basic

--- app.py
+++ app.py
@@ -7,16 +7,34 @@ import jwt
 
 app = Flask(__name__)
 
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
+
+def authenticate():
+    """Sends a 401 response that enables basic auth"""
+    return Response(
+        'Could not verify your access level for that URL.\n'
+        'You have to login with proper credentials', 401,
+        {'WWW-Authenticate': 'Basic realm="Login Required"'})
+
 @app.route("/check")
 def check():
-    try:
-        if session['logged_in']:
-            return Response('Login OK', 200, {})
-        else:
-            return Response('Login Required', 401, {})
-    except:
-        session['logged_in'] = False
-        return Response('Login Required', 401, {})
+    auth = request.authorization
+    if not auth or not check_auth(auth.username, auth.password):
+        return authenticate()
+    # try:
+    #     if session['logged_in']:
+    #         return Response('Login OK', 200, {})
+    #     else:
+    #         return Response('Login Required', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    # except:
+    #     session['logged_in'] = False
+    #     return Response('Login Required', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
 
 @app.route("/login")
 def login():
