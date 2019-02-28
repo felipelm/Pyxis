b2b81ece14043be3d9649a653c6d4fe1db473255
“felipelm” <felipelm123@gmail.com>
2019-02-28 13:26:27 -0300

    adding ldap

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, Response, render_template, flash, session, request, red
 import os
 import datetime
 import jwt
+import ldap
 
 app = Flask(__name__)
 
@@ -12,7 +13,7 @@ def check_auth(username, password):
     """This function is called to check if a username /
     password combination is valid.
     """
-    return username == 'admin' and password == 'secret'
+    return username == '123' and password == '123'
 
 
 def authenticate():
@@ -27,35 +28,20 @@ def check():
     auth = request.authorization
     if not auth or not check_auth(auth.username, auth.password):
         return authenticate()
-    # try:
-    #     if session['logged_in']:
-    #         return Response('Login OK', 200, {})
-    #     else:
-    #         return Response('Login Required', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
-    # except:
-    #     session['logged_in'] = False
-    #     return Response('Login Required', 401, {'WWW-Authenticate': 'Basic realm="Login Required"'})
+    return Response('Login OK', 200, {})
 
-@app.route("/login")
-def login():
-    rd = request.args.get('rd')
-
-    return render_template('login.html', rd=rd)
-
-
-@app.route('/login', methods=['POST'])
-def do_login():
-    if request.form['password'] == '123' and request.form['username'] == '123':
-        session['logged_in'] = True
-        return redirect(request.form['rd'], code=302)
-    else:
-        flash('wrong password!')
-    return check()
-
-@app.route("/logout")
-def logout():
-    session['logged_in'] = False
-    return check()
+@app.route("/ldap")
+def ldap_auth():
+  try:
+    #if authentication successful, get the full user data
+    connect.bind_s(user_dn,password)
+    result = connect.search_s(base_dn,ldap.SCOPE_SUBTREE,search_filter)
+    # return all user data results
+    connect.unbind_s()
+    return result
+  except ldap.LDAPError:
+    connect.unbind_s()
+    return "authentication error"
 
 if __name__ == '__main__':
     app.secret_key = '29cSy004wj2931m'
